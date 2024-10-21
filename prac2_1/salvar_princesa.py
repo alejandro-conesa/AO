9ccab84e572a53985bf79a368106be2209589807
Alex <acm382@alu.ua.es>
2024-10-21 21:18:44 +0200

    salvarprincesa

--- prac2_1/salvar_princesa.py
+++ prac2_1/salvar_princesa.py
@@ -8,14 +8,18 @@ def salvar_princesa(mazmorra):
         if i == mazmorra.shape[0] - 1 and j == mazmorra.shape[1] -1:
             return max(1, 1 - mazmorra[i, j])
         
-        if i >= mazmorra.shape[0] - 1 or j >= mazmorra.shape[1] -1:
+        if i > mazmorra.shape[0] - 1 or j > mazmorra.shape[1] -1:
             return math.inf
         
-        
+        abajo = salvar_princesa_custom(mazmorra, i+1, j)
+        derecha = salvar_princesa_custom(mazmorra, i, j+1)
+        vida = max(1, 1 - mazmorra[i, j])
+
+        return min(abajo, derecha) + vida
 
 
     return salvar_princesa_custom(mazmorra, i, j)
 
 
-mazmorra = np.array([[-7, -2], [1,]])
+mazmorra = np.array([[-2, -3, 3], [-5, -10, 1], [10, 30, -5]])
 print(salvar_princesa(mazmorra))
