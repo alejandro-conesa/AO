33283f3dc4943b5492c6e6d1689c8e20f6e2cbd5
Alex <acm382@alu.ua.es>
2024-10-23 20:05:57 +0200

    rework gira y gira memo (no funciona)

--- prac2_1/planear_gira.py
+++ prac2_1/planear_gira.py
@@ -1,13 +1,39 @@
-def planear_gira(C, R, entradas, d = 0):
-    if C == 0 or d >= len(entradas):
-        return 0
+import numpy as np
+
+def planear_gira(C, R, entradas):
+    dias = len(entradas)
+    def mochila(d, c, tickets, r):
+        if C == 0 or d <= 0:
+            return 0
+        
+        pos1 = mochila(d-r, c-1, tickets, r) + tickets[d-1]
+        pos2 = mochila(d-1, c, tickets, r)
+
+        return max(pos1, pos2)
     
-    pos1 = planear_gira(C-1, R, entradas, d+R) + entradas[d]
-    pos2 = planear_gira(C, R, entradas, d+1)
+    return mochila(dias, C, entradas, R)
 
-    return max(pos1, pos2)
+def planear_gira_memo(C, R, entradas):
+    d = len(entradas)
+    pd = np.full((d+1, C+1), -1)
 
-print(planear_gira(3, 3, [3000, 6000, 7000, 8000, 9000]))
+    def pgm(d_pgm, C_pgm, R_pgm, entradas_pgm, pd_pgm):
+        if pd_pgm[d_pgm, C_pgm] != -1:
+            return pd_pgm[d_pgm, C_pgm]
+        
+        if C_pgm == 0 or d_pgm <= 0:
+            pd_pgm[d_pgm, C_pgm] = 0 
+            return pd_pgm[d_pgm, C_pgm]
+        
+        pos1 = pgm(d_pgm-R_pgm, C_pgm-1, R_pgm, entradas_pgm, pd_pgm) + entradas_pgm[d-1]
+        pos2 = pgm(d_pgm-1, C_pgm, R_pgm, entradas_pgm, pd_pgm)
+
+        pd_pgm[d_pgm, C_pgm] = max(pos1, pos2)
+        return pd_pgm[d_pgm, C_pgm]
+
+    return pgm(d, C, R, entradas, pd)
+
+print(planear_gira_memo(3, 3, [3000, 6000, 7000, 8000, 9000]))
     
 
     
